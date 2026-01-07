abf5bf1a947cae10882215410969dd9592fd8b54
prudentelias1024 <119433507+prudentelias1024@users.noreply.github.com>
2026-01-07 22:49:57 +0100

    Update PDFPreviewer.tsx

--- app/components/PDFPreviewer.tsx
+++ app/components/PDFPreviewer.tsx
@@ -94,11 +94,9 @@ const renderImage = async (pdf, pageNum, temp) => {
 
   return (
     <div className="w-full h-screen ml-[2em]">
-      {/* {imgSrc !== ''  ?  */}
+     { pdfImages.length > 0   ?  
        <Fade easing='ease'>
-         {pdfImages.length > 0 ? 
-         
-         pdfImages.map((slideImage, index)=> {
+         { pdfImages.map((slideImage, index)=> {
         
            return (<div key={index}>
              {/* <img src={slideImage} alt="Preview PDF" className="w-full h-full ml-[2em] pl-[6em] shadow " /> */}
@@ -108,25 +106,26 @@ const renderImage = async (pdf, pageNum, temp) => {
             </div>
              </div>)
 })
-         :'' 
-         }  
+}
         </Fade>
      
+        :'' 
+        }  
        
             {/* <div className=' w-[90%] h-screen border rounded-md bg-gray-200 animate-pulse border-gray-300 ml-[7em] mt-[2em]'>
-
-             <svg
-          className="w-10 h-10 text-gray-100 mx-auto mt-[30%] align-middle dark:text-gray-600"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 20 18"
-        >
-          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-        </svg>
-    </div>
-
-      } */}
+              
+              <svg
+              className="w-10 h-10 text-gray-100 mx-auto mt-[30%] align-middle dark:text-gray-600"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 18"
+              >
+              <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+              </svg>
+              </div>
+              
+              } */}
     </div>
   );
 }
