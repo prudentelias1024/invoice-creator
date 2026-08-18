5ffa598e2f811099202d00239294ffd2fe12922e
prudentelias1024 <119433507+prudentelias1024@users.noreply.github.com>
2026-08-18 16:32:50 +0100

    updated some packages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["uutgahcujgkdyqnhmkww.supabase.co"]
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uutgahcujgkdyqnhmkww.supabase.co', // Replace with your image host domain
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    // domains: ["uutgahcujgkdyqnhmkww.supabase.co"]
   },
  
 };
