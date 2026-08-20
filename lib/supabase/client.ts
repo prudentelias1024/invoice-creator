6dc1eba7ec93e5cafb256b3fc1c20269e0415845
prudentelias1024 <119433507+prudentelias1024@users.noreply.github.com>
2026-08-20 14:40:20 +0100

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,9 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const client = createClient(
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
-export default client
+export default supabase
